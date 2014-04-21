9955dd90b78d7928ba3aa85e5c9f61fb4427f247
Denis Ziegler <dzincolorado@yahoo.com>
2014-04-20 23:30:13 -0400

    added genres function to moviedb.js

--- data/moviedb.js
+++ data/moviedb.js
@@ -1,1 +1,19 @@
 //functions to pull movie data from orchestrate
+
+var settings = require("../utility/settings");
+var orchestrate = require("orchestrate")(settings.getSettings.orchestrateKey());
+
+//get list of genres
+exports.genres = function(response){
+    orchestrate.list("genres")
+        .then(function(result){
+            var genres = result.body.results;
+            
+            response.render("genre", {
+                genres : genres
+            });
+            
+        }).fail(function(err){
+            
+        });   
+}
